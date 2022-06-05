b2739232c8b9a4f89675425a3ac728c9c4b0f59b
Samuel Banya <sbanya@fastmail.com>
2022-06-05 17:47:14 -0400

    Adding latest changes, did what I could since 'learn test' doesn't even support private methods

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius * this.radius;
+  }
+
+  get circumference() {
+    return 2 * Math.pi * this.radius;
+  }
+
+  get area() {
+    return Math.pi * (this.radius * this.radius);
+  }
+
+  set diameter(radius) {
+    this.diameter = this.radius * this.radius;
+  }
+
+  set circumference(radius) {
+    this.circumference = 2 * Math.pi * this.radius;
+  }
+
+  set area(radius) {
+    this.area = Math.pi * (this.radius * this.radius);
+  }
+}
+
