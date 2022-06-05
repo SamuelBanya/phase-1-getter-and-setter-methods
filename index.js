f2b142da5e62e9bba1c280683741987e5e16aa09
Samuel Banya <sbanya@fastmail.com>
2022-06-05 17:48:30 -0400

    Adding '#' pound signs for consistency

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 class Circle {
+  #radius;
+
   constructor(radius) {
-    this.radius = radius;
+    this.#radius = radius;
   }
 
   get diameter() {
-    return this.radius * this.radius;
+    return this.#radius * this.#radius;
   }
 
   get circumference() {
-    return 2 * Math.pi * this.radius;
+    return 2 * Math.pi * this.#radius;
   }
 
   get area() {
-    return Math.pi * (this.radius * this.radius);
+    return Math.pi * (this.#radius * this.#radius);
   }
 
   set diameter(radius) {
-    this.diameter = this.radius * this.radius;
+    this.diameter = this.#radius * this.#radius;
   }
 
   set circumference(radius) {
-    this.circumference = 2 * Math.pi * this.radius;
+    this.circumference = 2 * Math.pi * this.#radius;
   }
 
   set area(radius) {
-    this.area = Math.pi * (this.radius * this.radius);
+    this.area = Math.pi * (this.#radius * this.#radius);
   }
 }
-
